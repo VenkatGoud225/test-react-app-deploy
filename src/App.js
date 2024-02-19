f8ec44d4f3c9635d1607f90ee8ce068f6eeb1a88
Venkat Goud <goudnarasimhulu@gmail.com>
2024-02-19 07:54:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,14 @@ import SearchComponent from './SearchComponent';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div><h2>Goud</h2></div>
-      <SearchComponent/>
+     <>
+    <div className='App'>
+    <header className="App-header">
+        <h1>Bing Search App</h1>
+        <SearchComponent/>
+      </header>
     </div>
+    </>
   );
 }
 
